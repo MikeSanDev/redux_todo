91c5b805df2dbf0722522b5a7e98da2b45922f0e
MikeSanDev <mjs070594@gmail.com>
2023-04-26 21:33:34 -0500

    dispatch and reducers

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,14 +1,32 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { toggleComplete, deleteTodo } from '../redux/todoSlice';
 
 const TodoItem = ({ id, title, completed }) => {
+	const dispatch = useDispatch();
+
+	const handleComplete = () => {
+		dispatch(
+			toggleComplete({id: id, completed: !completed})
+		);
+	};
+
+	const handleDelete = () => {
+		dispatch(deleteTodo({id: id}))
+	}
 	return (
 		<li className={`list-group-item ${completed && 'list-group-item-success'}`}>
 			<div className='d-flex justify-content-between'>
 				<span className='d-flex align-items-center'>
-					<input type='checkbox' className='mr-3' checked={completed}></input>
+					<input 
+					type='checkbox' 
+					className='mr-3 checkbox' 
+					checked={completed}
+					onClick={handleComplete}
+					></input>
 					{title}
 				</span>
-				<button className='btn btn-danger'>Delete</button>
+				<button onClick={handleDelete} className='btn btn-danger'>Delete</button>
 			</div>
 		</li>
 	);
